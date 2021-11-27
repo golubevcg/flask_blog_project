b6cc0ff76e948e79b0cbe637e567c9ac23f93b8a
Andrew Golubev <golubevcg@gmail.com>
2021-11-28 01:58:36 +0300

    added about page, js scripts moved into separate js file, working on new article page

--- src/view/js/main.js
+++ src/view/js/main.js
@@ -8,3 +8,30 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-theme");
 }
+
+
+const { Editor } = toastui;
+const { chart, codeSyntaxHighlight, colorSyntax, tableMergedCell, uml } = Editor.plugin;
+
+const chartOptions = {
+  minWidth: 100,
+  maxWidth: 600,
+  minHeight: 100,
+  maxHeight: 300
+};
+
+const editor = new Editor({
+  el: document.querySelector('#editor'),
+  previewStyle: 'vertical',
+  height: '500px',
+  initialValue: allPluginsContent,
+  plugins: [[chart, chartOptions], [codeSyntaxHighlight, { highlighter: Prism }], colorSyntax, tableMergedCell, uml]
+});
+
+const viewer = Editor.factory({
+  el: document.querySelector('#viewer'),
+  viewer: true,
+  height: '500px',
+  initialValue: allPluginsContent,
+  plugins: [[chart, chartOptions], [codeSyntaxHighlight, { highlighter: Prism }], tableMergedCell, uml]
+});

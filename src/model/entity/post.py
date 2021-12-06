498f65dcc68c59ccc98628d531ac32b097eba8be
Andrew Golubev <golubevcg@gmail.com>
2021-12-07 02:22:00 +0300

    added changes into switching color theme, added optional post constructor arguments is_published and is_link_access

--- src/model/entity/post.py
+++ src/model/entity/post.py
@@ -19,7 +19,7 @@ class Post(db.Model):
     is_link_access = Column(Boolean, nullable=False, default=False)
     is_deleted = Column(Boolean, nullable=False, default=False)
 
-    def __init__(self, header: str, body: str):
+    def __init__(self, header: str, body: str, is_published: bool = False, is_link_access: bool = False):
         validate_input(header, str, "header")
         validate_input(body, str, "body")
 
@@ -29,6 +29,11 @@ class Post(db.Model):
         now = datetime.now()
         self.creation_date = now.strftime("%d/%m/%Y %H:%M:%S")
         self.is_deleted = False
+        if is_published:
+            self.is_published = is_published
+
+        if is_link_access:
+            self.is_link_access = is_link_access
 
     def __str__(self):
         return f"Post(id:{self.id})"

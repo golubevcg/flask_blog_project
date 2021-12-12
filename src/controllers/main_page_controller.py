b9f60d459e07af7540674c7549d5383b19d97e9f
Andrew Golubev <golubevcg@gmail.com>
2021-12-12 23:54:00 +0300

    removed search

--- src/controllers/main_page_controller.py
+++ src/controllers/main_page_controller.py
@@ -19,7 +19,14 @@ main_page_blueprint = Blueprint(
 @main_page_blueprint.route("/")
 @main_page_blueprint.route("/<page>")
 def main(page=1):
-    all_posts = post_dao.get_all_published_posts_from_page(1)
+    if type(page) != int:
+        try:
+            page = int(page)
+        except Exception as e:
+            main_logger.exception(e)
+            return
+
+    all_posts = post_dao.get_all_published_posts_from_page(page)
 
     posts_years = [post.creation_date.year for post in all_posts]
     temp_year = None
@@ -35,13 +42,13 @@ def main(page=1):
             temp_year = year
 
     posts_amount = post_dao.get_posts_amount()
-    pages_amount = int( math.ceil(posts_amount // 10) )
+    pages_amount = math.ceil(posts_amount / 10 )
 
     return render_template("main_page.html",
                            all_posts=all_posts,
                            posts_years=posts_years,
                            pages_amount=pages_amount,
-                           page=page)
+                           page_num=page)
 
 
 @main_page_blueprint.route("/about.html")
@@ -79,39 +86,3 @@ def login_post():
     else:
         login_user(user)
         return redirect("/")
-
-
-@main_page_blueprint.route("/get_posts_from_page_num/<page_num>", methods=['POST'])
-def get_posts_from_page_num(page_num):
-    if not page_num:
-        return ""
-
-    try:
-        page_num = int(page_num)
-    except Exception as e:
-        main_logger.log(e)
-        return ""
-
-    posts_on_page = post_dao.get_all_published_posts_from_page(page_num)
-    posts_years = [post.creation_date.year for post in posts_on_page]
-
-    posts_on_page = [post.to_dict() for post in posts_on_page]
-
-    temp_year = None
-    for i in range(len(posts_years)):
-        year = posts_years[i]
-        if not temp_year:
-            temp_year = year
-            continue
-
-        if year == temp_year:
-            posts_years[i] = ""
-        elif year != temp_year:
-            temp_year = year
-
-    posts_amount = post_dao.get_posts_amount()
-    pages_amount = int(math.ceil(posts_amount // 10))
-    result = {"all_posts": posts_on_page, "post_years": posts_years, "pages_amount": pages_amount}
-    result = json.dumps(result)
-
-    return result

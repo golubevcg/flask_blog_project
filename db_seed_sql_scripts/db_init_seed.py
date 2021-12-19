540a941cae2d1ef2945c2d151b4a73598e4ac33a
Andrew Golubev <golubevcg@gmail.com>
2021-12-19 18:26:40 +0300

    finished with seeding data

---
+++ db_seed_sql_scripts/db_init_seed.py
@@ -0,0 +1,17 @@
+from main import app, db
+
+# seeding data
+with app.app_context():
+    existing_post = db.session().execute("SELECT * FROM public.posts WHERE id=3")
+    if not existing_post:
+        sql_posts_init = open("db_seed_sql_scripts/init_posts.sql")
+        db.session().execute(sql_posts_init.read())
+        db.session.commit()
+        sql_posts_init.close()
+
+    admin_user = db.session().execute("SELECT * FROM public.users WHERE login='admin'")
+    if not admin_user:
+        sql_users_init = open("db_seed_sql_scripts/init_users.sql")
+        db.session().execute(sql_users_init.read())
+        db.session.commit()
+        sql_users_init.close()
--- prestart.sh
+++ prestart.sh
@@ -6,4 +6,10 @@ FLASK_APP=main flask db upgrade
 
 echo migrations ok
 
+echo Init db data
+
+python db_seed_sql_scripts/db_init_seed.py
+
+echo db data ok
+
 exec "$@"

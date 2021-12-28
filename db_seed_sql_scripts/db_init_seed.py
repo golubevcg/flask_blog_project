c06d31b26d97e1047b0411891086a1162d8a4034
Andrew Golubev <golubevcg@gmail.com>
2021-12-28 23:25:44 +0300

    removed init posts

--- db_seed_sql_scripts/db_init_seed.py
+++ db_seed_sql_scripts/db_init_seed.py
@@ -3,15 +3,6 @@ from services.logger_service import main_logger
 
 # seeding data
 with app.app_context():
-    existing_post1 = db.session().execute("SELECT * FROM public.posts WHERE id=12").first()
-    existing_post2 = db.session().execute("SELECT * FROM public.posts WHERE id=13").first()
-
-    if not existing_post1 or not existing_post2:
-        sql_posts_init = open("db_seed_sql_scripts/init_posts.sql")
-        db.session().execute(sql_posts_init.read())
-        db.session.commit()
-        sql_posts_init.close()
-
     admin_user = db.session().execute("SELECT * FROM public.users WHERE login='admin'").first()
     if not admin_user:
         sql_users_init = open("db_seed_sql_scripts/init_users.sql")

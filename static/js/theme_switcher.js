6e152198bcfbcba7aa1b4e46b20b155283339f52
Andrei Golubev <golubevcg@gmail.com>
2023-12-27 12:20:18 +0000

    Finished working on main page

--- static/js/theme_switcher.js
+++ static/js/theme_switcher.js
@@ -1,14 +1,18 @@
-// Theme update function called when theme switcher clicked
 function switch_theme() {
     // Determine the current theme
     let current_theme = localStorage.getItem("theme");
-    // Toggle the theme between 'light' and 'dark'
-    let updated_theme = (current_theme === "dark") ? "root" : "dark";
+
+    // If the current theme is 'light', switch to 'dark' (which is defined by :root styles)
+    // Otherwise, switch to 'light'
+    let updated_theme = (current_theme === "light") ? "dark" : "light";
     
     // Update the theme in localStorage
     localStorage.setItem("theme", updated_theme);
-    
+
     // Apply the new theme
-    document.body.classList.toggle("dark-theme", updated_theme === "dark");
-    console.log("This will be printed to the console");
+    if (updated_theme === "light") {
+        document.body.classList.add("light-theme");
+    } else {
+        document.body.classList.remove("light-theme");
+    }
 }
